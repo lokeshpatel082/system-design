47aa78a12f4e867a64d60b57296471ef8e95a38e
Lokesh <lokeshpatel082@gmail.com>
2024-10-11 14:00:51 +0530

    strategy desing pattern complete

--- design-patterns/src/main/java/com/loki/design/patterns/strategy/ShoppingCart.java
+++ design-patterns/src/main/java/com/loki/design/patterns/strategy/ShoppingCart.java
@@ -2,10 +2,11 @@ package com.loki.design.patterns.strategy;
 
 public class ShoppingCart {
     private PaymentStrategy paymentStrategy;
-    public ShoppingCart(PaymentStrategy paymentStrategy){
+    public ShoppingCart(){
+    }
+    public void setPaymentStrategy(PaymentStrategy paymentStrategy){
         this.paymentStrategy = paymentStrategy;
     }
-
     public void checkout(int amount){
         paymentStrategy.pay(amount);
     }
--- design-patterns/src/main/java/com/loki/design/patterns/strategy/StrategyDesignPatternDemo.java
+++ design-patterns/src/main/java/com/loki/design/patterns/strategy/StrategyDesignPatternDemo.java
@@ -2,4 +2,18 @@ package com.loki.design.patterns.strategy;
 
 public class StrategyDesignPatternDemo {
 
+    public static void main(String[] args){
+        PaymentStrategy paymentStrategy1 = new CreditCardPaymentStrategy("89XXXX90","Lokesh","897","89-90-90");
+        ShoppingCart cart = new ShoppingCart();
+        cart.setPaymentStrategy(paymentStrategy1);
+        cart.checkout(100);
+
+        PaymentStrategy paymentStrategy2 = new PaypalPaymentStrategy("userName","0q9834093843");
+        cart.setPaymentStrategy(paymentStrategy2);
+        cart.checkout(100);
+
+        PaymentStrategy paymentStrategy3 = new BitcoinPaymentStrategy("0q9834093843");
+        cart.setPaymentStrategy(paymentStrategy3);
+        cart.checkout(100);
+    }
 }
