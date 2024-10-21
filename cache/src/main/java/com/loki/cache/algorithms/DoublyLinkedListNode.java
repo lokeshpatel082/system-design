81702b71da7973222f63fa8b81c436988e80d124
Lokesh <lokeshpatel082@gmail.com>
2024-10-21 10:32:52 +0530

    DoublyLinkedList implementation complete for LRU eviction policy

---
+++ cache/src/main/java/com/loki/cache/algorithms/DoublyLinkedListNode.java
@@ -0,0 +1,16 @@
+package com.loki.cache.algorithms;
+
+import lombok.Getter;
+
+@Getter
+public class DoublyLinkedListNode<E> {
+    E element;
+    DoublyLinkedListNode next;
+    DoublyLinkedListNode prev;
+
+    public DoublyLinkedListNode(E element){
+        this.element = element;
+        this.next = null;
+        this.prev = null;
+    }
+}
